00c9180f1d1b8271635a434644dd841d423d1a83
Matthew Glasenapp <mglasena@ucsc.edu>
2022-07-14 10:07:09 -0700

    Update preprocess.py

--- scripts/preprocess_sequencing_data/preprocess.py
+++ scripts/preprocess_sequencing_data/preprocess.py
@@ -55,9 +55,9 @@ dict = {
 "SRR5767281" : ["SRR5767281","nudus","QB3KMK011","SAMN07269101","VJCQB3KMK011","HS2:148:C0EN2ACXX:4","C0EN2ACXX:4"],
 "SRR5767282" : ["SRR5767282","franciscanus","QB3KMK010","SAMN07269100","VJCQB3KMK010","HS2:148:C0EN2ACXX:5","C0EN2ACXX:5"],
 #"DRR107786" : ["DRR107786","pulcherrimus","SAMD00098133","SAMD00098133","DRR107786","HWI-ST462R:262:C1J2AACXX:3","C1J2AACXX:3"],
-"SRR5767284" : ["SRR5767284","depressus","QB3KMK015","SAMN07269098","VJCQB3KMK015","HS3:171:d0le4acxx:2","d0le4acxx:2"],
-"SRR5767285" : ["SRR5767285","pallidus","QB3KMK002","SAMN07269097","VJCQB3KMK002","HS1_0066:8","HS1_0066:8"],
-"SRR5767286" : ["SRR5767286","droebachiensis","QB3KMK014","SAMN07269096","VJCQB3KMK014","HS3:171:d0le4acxx:1","d0le4acxx:1"],
+#"SRR5767284" : ["SRR5767284","depressus","QB3KMK015","SAMN07269098","VJCQB3KMK015","HS3:171:d0le4acxx:2","d0le4acxx:2"],
+#"SRR5767285" : ["SRR5767285","pallidus","QB3KMK002","SAMN07269097","VJCQB3KMK002","HS1_0066:8","HS1_0066:8"],
+#"SRR5767286" : ["SRR5767286","droebachiensis","QB3KMK014","SAMN07269096","VJCQB3KMK014","HS3:171:d0le4acxx:1","d0le4acxx:1"],
 "SRR6281818" : ["SRR6281818","purpuratus","S.purpuratus#1","SAMN08013506","A630_1","SRR6281818","SRR6281818"],
 #"ERR5671699" : ["ERR5671699","lividus","4","ERS2351987","4089_HiC_PI_3","ERR5671699","ERR5671699"],
 "SRR5767283" : ["SRR5767283","pulcherrimus","QB3KMK016","SAMN07269099","VJCQB3KMK016","HS3:171:d0le4acxx:3","d0le4acxx:3"],
@@ -180,8 +180,8 @@ class Accessions:
 			os.system(mark_duplicates)
 
 			# Clean up intermediate_files
-			os.system("rm " + input_bam_1)
-			os.system("rm " + input_bam_2)
+			#os.system("rm " + input_bam_1)
+			#os.system("rm " + input_bam_2)
 
 		# No multiplex
 		else:
@@ -192,7 +192,7 @@ class Accessions:
 			os.system(mark_duplicates)
 
 			# Clean up intermediate_files
-			os.system("rm " + input_bam)
+			#os.system("rm " + input_bam)
 
 	def get_alignment_stats(self):
 		print("Samtools flagstat. Getting alignment metrics from clean BAM files.")
