5bd7f4ef19ecf36692f38d3d78647684e0a6ddd5
Matthew Glasenapp <mglasena@ucsc.edu>
2022-07-27 15:46:48 -0700

    Update parallel_single_locus_trees.py

--- scripts/iqtree/parallel_single_locus_trees.py
+++ scripts/iqtree/parallel_single_locus_trees.py
@@ -11,7 +11,7 @@ def iqtree(file):
     run_iqtree = "iqtree -s {}{} -m MFP".format(fasta_alignment_directory,file)
     os.system(run_iqtree)
 
-main():
+def main():
     Parallel(n_jobs=num_cores)(delayed(iqtree)(fasta_alignment_file) for fasta_alignment_file in fasta_alignment_file_list)
     
 if __name__ == "__main__":
