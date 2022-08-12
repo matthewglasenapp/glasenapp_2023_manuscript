12791a86875dd16eb1ecc9f204c46b98df286024
Matthew Glasenapp <mglasena@ucsc.edu>
2022-08-12 13:29:34 -0700

    Update run_mosdepth.sh

--- scripts/bam_coverage/run_mosdepth.sh
+++ scripts/bam_coverage/run_mosdepth.sh
@@ -1,1 +1,17 @@
-mosdepth --by regions.bed --no-per-base -t 4 --fast-mode --thresholds 10,20,30,50,100 franc /hb/scratch/mglasena/data/dedup_mapped_bam_files/franciscanus_SRR5767282_dedup_aligned_reads.bam
+#!/bin/bash
+#SBATCH --job-name=mosdepth
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mglasena@ucsc.edu
+#SBATCH --output=mosdepth_%j.out
+#SBATCH --nodes=1
+#SBATCH --partition=128x24
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=50GB
+#SBATCH --time=168:00:00
+
+module load python-3.6.5
+
+mosdepth --by regions.bed --no-per-base -t 4 --fast-mode --thresholds 10,20,30,50,100 droe /hb/groups/pogson_group/dissertation/data/dedup_mapped_bam_files/droebachiensis_SRR5767286_dedup_aligned_reads.bam
+
+python3 plot-dist.py droe.mosdepth.global.dist.txt
