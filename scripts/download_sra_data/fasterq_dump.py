41588a80411e1549e3c333ff790223a88098090f
Matthew Glasenapp <mglasena@ucsc.edu>
2022-08-19 16:35:15 -0700

    update fasterq_dump.py

--- scripts/download_sra_data/fasterq_dump.py
+++ scripts/download_sra_data/fasterq_dump.py
@@ -6,9 +6,14 @@ Download multiple accession from the NCBI SRA. Consider modifying to parallelize
 
 import os
 
+prefetch = "/hb/groups/pogson_group/dissertation/software/sratoolkit.2.11.2-centos_linux64/bin/prefetch"
 fasterq_dump = "/hb/groups/pogson_group/dissertation/software/sratoolkit.2.11.2-centos_linux64/bin/fasterq-dump"
 
 threads = 8
+
+prefetch_directory = "/hb/scratch/mglasena/prefetch/"
+make_prefetch_dir = "mkdir -p {}".format(prefetch_directory)
+os.system(make_prefetch_dir)
 
 output_directory = "/hb/home/mglasena/short_read_data/"
 make_output_dir = "mkdir -p {}".format(output_directory)
@@ -16,35 +21,56 @@ os.system(make_output_dir)
 
 temporary_directory = "/hb/scratch/mglasena/SRA/"
 
-prefetch_dir = "/hb/scratch/mglasena/"
-
-skipped_accessions = {"SRR5767283": "pulcherrimus,QB3KMK016,SAMN07269099", "SRR5767280": "intermedius,QB3KMK012,SAMN07269102"}
+# skipped_accessions = {"SRR5767283": "pulcherrimus,QB3KMK016,SAMN07269099", "SRR5767280": "intermedius,QB3KMK012,SAMN07269102"}
 
 # species, sample_name, ncbi biosample
 
-urchin_sra_accessions = {
+#urchin_sra_accessions = {
 	#"SRR5767279": "fragilis,QB3KMK013,SAMN07269103",
 	#"SRR5767281": "nudus,QB3KMK011,SAMN07269101",
 	#"SRR5767282": "franciscanus,QB3KMK010,SAMN07269100",
-	"DRR107784": "pulcherrimus,SAMD00098133,SAMD00098133",
+	#"DRR107784": "pulcherrimus,SAMD00098133,SAMD00098133",
 	#"SRR5767284": "depressus,QB3KMK015,SAMN07269098",
 	#"SRR5767285": "pallidus,QB3KMK002,SAMN07269097",
 	#"SRR5767286": "droebachiensis,QB3KMK014,SAMN07269096",
 	#"SRR6281818": "purpuratus,S.purpuratus#1,SAMN08013506",
 	#"SRR7211988": "purpuratus,SPUR.00,SAMN00829422",
 	#"ERR5621404": "lividus,4,ERS2351987"
-	#"SRR2583947": "franciscanus,Sf1,SAMN04156173"
+	#"SRR2583947": "franciscanus,Sf1,SAMN04156173"}
+
+urchin_sra_accessions = {
+	#"ERR5621404": "lividus", "4", "ERS2351987",
+	"ERR5621405": "lividus,4,ERS2351987",
+	"ERR5621406": "lividus,4,ERS2351987",
+	"ERR5621407": "lividus,4,ERS2351987",
+	"ERR5621408": "lividus,4,ERS2351987",
+	"ERR5621409": "lividus,4,ERS2351987",
+	"ERR5621410": "lividus,4,ERS2351987",
+	"ERR5621411": "lividus,4,ERS2351987",
+	"ERR5621412": "lividus,4,ERS2351987",
+	"ERR5621413": "lividus,4,ERS2351987",
+	"ERR5621414": "lividus,4,ERS2351987",
+	"ERR5621415": "lividus,4,ERS2351987",
+	"ERR5621416": "lividus,4,ERS2351987",
+	"ERR5621417": "lividus,4,ERS2351987",
+	"ERR5621418": "lividus,4,ERS2351987",
 }
 
+def prefetch(accession_number):
+	output_dir = prefetch_directory + accession_number
+	"{} {} -O {} --max-size u".format(prefetch, accession_number, output_dir)
+
 def download_accession(accession_number):
-	os.chdir(prefetch_dir + accession_number)
+	os.chdir(prefetch_directory + accession_number)
 	temp_dir = temporary_directory + accession_number 
-	download = "{} {} --outdir {} --split-3 -t {} -e {}".format(fasterq_dump, accession_number, output_directory, temp_dir, threads, prefetch_dir)
+	download = "{} {} --outdir {} --split-3 -t {} -e {}".format(fasterq_dump, accession_number, output_directory, temp_dir, threads)
 
 	os.system(download)
 
 def main():
 	for key in urchin_sra_accessions:
+		print("prefetching {}".format(key))
+		prefetch(key)
 		print("downloading {}".format(key))
 		download_accession(key)
 		print("{} complete".format(key))
