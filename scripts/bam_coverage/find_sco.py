84258880a79203b0531fb68745559c216593b4a6
Matthew Glasenapp <mglasena@ucsc.edu>
2022-08-26 11:00:52 -0700

    Update find_sco.py

--- scripts/bam_coverage/find_sco.py
+++ scripts/bam_coverage/find_sco.py
@@ -11,6 +11,7 @@ min_cov_threshold = 10
 max_cov_threshold = 100
 prop_1x_threshold = 0.5
 
+lividus_min = 5
 purpuratus_7211988_max = 150
 pulcherrimus_DRR107784_max = 150
 
@@ -19,11 +20,11 @@ gene_dict = dict()
 passed_genes_dict = dict()
 
 def get_zipped_bed_file_list():
-	#get_regions_file_paths = "find {} -type f -name *.regions.bed.gz* | grep -v 'csi' > regions_files".format(bed_file_dir)
-	#os.system(get_regions_file_paths)
+	get_regions_file_paths = "find {} -type f -name *.regions.bed.gz* | grep -v 'csi' > regions_files".format(bed_file_dir)
+	os.system(get_regions_file_paths)
 
-	#get_thresholds_file_paths = "find {} -type f -name *.thresholds.bed.gz* | grep -v 'csi' > thresholds_files".format(bed_file_dir)
-	#os.system(get_thresholds_file_paths)
+	get_thresholds_file_paths = "find {} -type f -name *.thresholds.bed.gz* | grep -v 'csi' > thresholds_files".format(bed_file_dir)
+	os.system(get_thresholds_file_paths)
 
 	with open("regions_files", "r") as f1, open("thresholds_files","r") as f2:
 		file_list = zip(sorted(f1.read().splitlines()),sorted(f2.read().splitlines()))
@@ -31,10 +32,12 @@ def get_zipped_bed_file_list():
 	# Get index of purpuratus_7211988 and pulcherrimus_DRR107784 in created lists
 	global purpuratus_7211988_index	
 	global pulcherrimus_DRR107784_index
+	global lividus_index
 	with open("regions_files","r") as f:
 		order = sorted(f.read().splitlines())
 		purpuratus_7211988_index = [order.index(item) for item in order if "purpuratus_SRR7211988" in item][0]
 		pulcherrimus_DRR107784_index = [order.index(item) for item in order if "pulcherrimus_DRR107784" in item][0]
+		lividus_index = [order.index(item) for item in order if "lividus_ERS2351987" in item][0]
 
 	return list(file_list)
 
@@ -73,7 +76,9 @@ def filter_gene_dict():
 		pulcherrimus_DRR107784_mean = mean_depth_lst.pop(pulcherrimus_DRR107784_index)
 		pulcherrimus_DRR107784_1x = one_x_lst.pop(pulcherrimus_DRR107784_index)
 		
-		if min(mean_depth_lst) >= min_cov_threshold and max(mean_depth_lst) < max_cov_threshold and min(one_x_lst) >= prop_1x_threshold and purpuratus_7211988_mean >= min_cov_threshold and purpuratus_7211988_mean < purpuratus_7211988_max and purpuratus_7211988_1x >= prop_1x_threshold and pulcherrimus_DRR107784_mean >= min_cov_threshold and pulcherrimus_DRR107784_mean < pulcherrimus_DRR107784_max and pulcherrimus_DRR107784_1x >= prop_1x_threshold:
+		lividus_mean = mean_depth_lst.pop(lividus_index)
+		
+		if min(mean_depth_lst) >= min_cov_threshold and max(mean_depth_lst) < max_cov_threshold and min(one_x_lst) >= prop_1x_threshold and purpuratus_7211988_mean >= min_cov_threshold and purpuratus_7211988_mean < purpuratus_7211988_max and purpuratus_7211988_1x >= prop_1x_threshold and pulcherrimus_DRR107784_mean >= min_cov_threshold and pulcherrimus_DRR107784_mean < pulcherrimus_DRR107784_max and pulcherrimus_DRR107784_1x >= prop_1x_threshold and lividus_mean >= lividus_min:
 			
 			passed_genes_dict[key] = value
 
@@ -92,7 +97,7 @@ def write_genes_passed_filter_bed():
 def write_all_gene_dict_csv():
 	csv_file = open("all_genes.csv","w")
 	writer = csv.writer(csv_file)	
-	header = ["gene", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988"]
+	header = ["gene", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "lividus", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988"]
 	writer.writerow(header)
 
 	for key,value in gene_dict.items():
@@ -104,7 +109,7 @@ def write_all_gene_dict_csv():
 def write_passed_genes_dict_csv():
 	csv_file = open("passed_genes.csv","w")
 	writer = csv.writer(csv_file)	
-	header = ["gene", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988"]
+	header = ["gene", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "lividus", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988", "depressus", "droebachiensis", "fragilis", "franciscanus", "intermedius", "nudus", "pallidus", "pulcherrimus_DRR107784", "pulcherrimus_SRR5767283", "purpuratus_SRR6281818", "purpuratus_SRR7211988"]
 	writer.writerow(header)
 
 	for key,value in passed_genes_dict.items():
