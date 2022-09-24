89135462eb03c7f0680caf8e6c08c8b0e5b1d858
Matthew Glasenapp <mglasena@ucsc.edu>
2022-09-24 17:16:45 +1000

    Update parse_iqtree_log.py

--- scripts/create_gene_trees/parse_iqtree_log.py
+++ scripts/create_gene_trees/parse_iqtree_log.py
@@ -24,28 +24,36 @@ for item in lines:
 	if item[0:22] == "Reading alignment file":
 		index = lines.index(item)
 		gene = item.split("/")[1].split(".fas")[0]
-		print(gene)
 		gene_file = item.split("file ")[1].split(" ...")[0]
-		print(gene_file)
 		number_sites = lines[index+4].split(" ")[5]
-		print(number_sites)
 		parsimony_informative_sites = lines[index + 5].split(" ")[0]
-		print(parsimony_informative_sites)
+		gap_ambiguity_lst = []
 		gap_ambiguity_1 = lines[index+9].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_1)))
 		gap_ambiguity_2 = lines[index+10].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_2)))
 		gap_ambiguity_3 = lines[index+11].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_3)))
 		gap_ambiguity_4 = lines[index+12].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_4)))
 		gap_ambiguity_5 = lines[index+13].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_5)))
 		gap_ambiguity_6 = lines[index+14].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_6)))
 		gap_ambiguity_7 = lines[index+15].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_7)))
 		gap_ambiguity_8 = lines[index+16].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_8)))
 		gap_ambiguity_9 = lines[index+17].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_9)))
 		gap_ambiguity_10 = lines[index+18].split("%")[0].split(" ")[-1]
+		gap_ambiguity_lst.append(int(float(gap_ambiguity_10)))
 
 		gene_list[gene] = [number_sites,parsimony_informative_sites,gap_ambiguity_1,gap_ambiguity_2,gap_ambiguity_3,gap_ambiguity_4]
 
-		if int(float(number_sites)) < 300 or int(float(parsimony_informative_sites)) == 0 or int(float(gap_ambiguity_1)) > 50 or int(float(gap_ambiguity_2)) > 50 or int(float(gap_ambiguity_3)) > 50 or int(float(gap_ambiguity_4)) > 50:
-			failed_genes[gene] = [number_sites,parsimony_informative_sites,gap_ambiguity_1,gap_ambiguity_2,gap_ambiguity_3,gap_ambiguity_4]
+		# int(float(number_sites)) < 300 or 
+		if int(float(parsimony_informative_sites)) == 0 or int(max(gap_ambiguity_lst)) > 50:
+			failed_genes[gene] = [number_sites,parsimony_informative_sites,gap_ambiguity_lst]
 			failed_gene_names.append(gene)
 		else:
 			passed_genes.append(gene_file)
@@ -64,6 +72,8 @@ passed_genes_file = open("passed_genes.txt","a")
 for gene in passed_genes:
 	passed_genes_file.write(gene + "\n")
 passed_genes_file.close()
+
+print(failed_genes)
 
 
 
