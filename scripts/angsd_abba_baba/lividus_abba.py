50adeba5ef2fa3eda50c54874a0ebfdb6967f624
Matthew Glasenapp <mglasena@ucsc.edu>
2022-10-05 14:19:45 -0700

    update

---
+++ scripts/angsd_abba_baba/lividus_abba.py
@@ -0,0 +1,21 @@
+import os
+
+reference_genome = "/hb/groups/pogson_group/dissertation/data/purpuratus_reference/GCF_000002235.5_Spur_5.0_genomic.fna"
+
+def run_abba_baba(dir):
+	os.chdir(dir)
+
+	angsd_command = "angsd -doAbbababa 1 -doCounts 1 -baq 1 -ref {} -useLast 1 -bam bam.filelist -out out -blockSize 1000000 -minMapQ 20 -minQ 20 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1".format(reference_genome)
+
+	jackknife = "Rscript jackKnife.R file=out.abbababa indNames=bam.filelistnames outfile=results"
+
+	os.system(angsd_command)
+	os.system(jackknife)
+
+def main():
+	run_abba_baba("/hb/home/mglasena/dissertation/data/angsd_abba_baba/lividus/all/")
+
+	print("Analysis Finished!")
+
+if __name__ == "__main__":
+	main()
