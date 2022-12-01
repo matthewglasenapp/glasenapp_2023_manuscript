315022dd4620b9b7ea1141efffb8ff3a7d8372af
Matthew Glasenapp <mglasena@ucsc.edu>
2022-12-01 09:53:58 -0800

    Update gcf.sh

--- scripts/iqtree/gcf.sh
+++ scripts/iqtree/gcf.sh
@@ -5,12 +5,13 @@
 #SBATCH --output=gcf_%j.out
 #SBATCH --nodes=1
 #SBATCH --partition=128x24
-#SBATCH --mem=10GB
-#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --mem=0   
+#SBATCH --time=7-0
 
-species_tree_file=""
-single_locus_trees_file=""
-fasta_alignment_dir=""
+species_tree_file="/hb/scratch/mglasena/strongylocentrotidae/species_tree/rev_dna.treefile"
+single_locus_trees_file="/hb/scratch/mglasena/strongylocentrotidae/loci.treefile"
+fasta_alignment_dir="/hb/scratch/mglasena/strongylocentrotidae/vcf2fasta_CDS/"
 
-
-iqtree -t $species_tree_file --gcf $single_locus_trees_file --cf-verbose --df-tree -p $fasta_alignment_dir --scf 100 --prefix concord -T 10
+iqtree -t $species_tree_file --gcf $single_locus_trees_file --cf-verbose --df-tree -p $fasta_alignment_dir --scf 300 --prefix concord -T 8
